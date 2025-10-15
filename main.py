b505a4c59a508e8074fe5dedd493e1c180637e90
000kirill <kryaka1108@gmail.com>
2025-10-15 21:38:21 +0500

    update main.py

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ from matplotlib import pyplot as plt
 
 
 def get_coordinates(city_name, country_code):
+    limit = 20
     url = "https://data.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-postal-code@public/records"
     payload = {
-    "limit": 20,
+    "limit": limit,
     "where": {f'place_name: "{city_name}" and country_code: "{country_code}"'},
     }
     response = requests.get(url, params=payload)
     response.raise_for_status()
-    results = (response.json()["results"][0])
+    total_count = response.json()['total_count']
+    if total_count == 0:
+        print('Город не найден!')
+        exit()
+    results = response.json()["results"][0]
     return results['latitude'], results['longitude']
 
 
@@ -28,12 +33,12 @@ def get_temp_statistics(latitude, longitude, start_date, end_date):
     response.raise_for_status()
     temperature_value = response.json()['hourly']['temperature_2m']
     tempеrature_date = response.json()['hourly']['time']
-    df = pd.DataFrame(list(zip(tempеrature_date, temperature_value)), columns =['date', 'temp'])
+    df = pd.DataFrame(list(zip(tempеrature_date, temperature_value)), columns=['date', 'temp'])
     return df
 
 
 def get_graph(df):
-    chart = df.plot(x='date', y='temp', kind='line')
+    df.plot(x='date', y='temp', kind='line')
     plt.xlabel('Дата')
     plt.ylabel('Температура (°C)')
     plt.show()
